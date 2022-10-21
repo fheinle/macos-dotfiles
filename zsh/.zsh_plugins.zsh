3ff972731d22d31d238f32efe3598eaa72689646
Florian Heinle <launchpad@planet-tiax.de>
2022-10-21 18:03:11 +0200

    zsh: add fzf plugin for completion

---
+++ zsh/.zsh_plugins.zsh
@@ -0,0 +1,2 @@
+fpath+=( /Users/florian/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab )
+source /Users/florian/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab/fzf-tab.plugin.zsh
